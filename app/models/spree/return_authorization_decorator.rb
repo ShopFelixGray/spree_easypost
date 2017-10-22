019fad37dd965e24e193089ae2f33d7b2f9b4a9a
hf2186 <hf2186@users.noreply.github.com>
2017-10-22 15:25:49 -0400

    Adding refund to cancel

--- app/models/spree/return_authorization_decorator.rb
+++ app/models/spree/return_authorization_decorator.rb
@@ -1,3 +1,18 @@
 Spree::ReturnAuthorization.class_eval do
   has_many :customer_shipments, class_name: "Spree::CustomerShipment", :dependent => :destroy
+
+  state_machine.before_transition(
+    to: :canceled,
+    do: :customer_shipments_refund_labels
+  )
+ 
+
+  def customer_shipments_refund_labels
+    if customer_shipments.any?
+      customer_shipments.each do |customer_shipment|
+        customer_shipment.refund_label
+      end
+    end
+  end
+
 end
