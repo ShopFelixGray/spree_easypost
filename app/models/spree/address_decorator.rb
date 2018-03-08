d78eb9342c328cf0dc2476acffe6f3c39103d104
hf2186 <hf2186@users.noreply.github.com>
2018-03-07 23:01:31 -0500

    Adding address validation

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -3,13 +3,14 @@ module Spree
     module AddressDecorator
       def easypost_address
         attributes = {
+          verify: ["delivery", "zip4"],
           street1: address1,
           street2: address2,
           city: city,
           zip: zipcode,
           phone: phone
         }
-
+        
         attributes[:company] = company if respond_to?(:company)
         attributes[:name] = full_name if respond_to?(:full_name)
         attributes[:state] = state ? state.abbr : state_name
