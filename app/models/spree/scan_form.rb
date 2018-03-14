e29cfb7afea74f2ac695a42d6a9f5af65c0dcc7c
hf2186 <hf2186@users.noreply.github.com>
2018-03-14 00:25:45 -0400

    Adding number to scan form

--- app/models/spree/scan_form.rb
+++ app/models/spree/scan_form.rb
@@ -1,5 +1,10 @@
 module Spree
     class ScanForm < Spree::Base
+        include Spree::Core::NumberGenerator.new(prefix: 'SF', length: 11)
+
+        extend FriendlyId
+        friendly_id :number, slug_column: :number, use: :slugged
+
         belongs_to :stock_location
         has_many :shipments
 
