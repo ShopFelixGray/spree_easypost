737b4ca297b97b7e1a64a24ee695a59e1d1a560c
hf2186 <hf2186@users.noreply.github.com>
2018-03-17 20:36:07 -0400

    Modal Removal

--- app/assets/javascripts/spree/backend/spree_easypost.js
+++ app/assets/javascripts/spree/backend/spree_easypost.js
@@ -1,7 +1,4 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 
-//= require spree/backend/create_scan_form
 //= require spree/backend/buy_postage
-
-Spree.routes.scan_form = Spree.pathFor('api/v1/scan_form')
