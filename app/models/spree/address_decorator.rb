f1950417b246e2e635357ef3efb00630dedd162b
Jared Murphy <jaredcmurphy@gmail.com>
2018-03-27 19:12:52 -0400

    FGR-1513 adds address verification functionality

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -10,7 +10,7 @@ module Spree
           zip: zipcode,
           phone: phone
         }
-        
+
         attributes[:company] = company if respond_to?(:company)
         attributes[:name] = full_name if respond_to?(:full_name)
         attributes[:state] = state ? state.abbr : state_name
@@ -18,9 +18,21 @@ module Spree
 
         ::EasyPost::Address.create attributes
       end
+
+      def easypost_address_validate
+        zip4 = easypost_address.verifications.zip4
+        delivery = easypost_address.verifications.delivery
+        failed_validations = !zip4.success || !delivery.success
+
+        if failed_validations
+          zip4.errors.each { |error| errors.add(:zipcode, error.message) }
+          delivery.errors.each { |error| errors.add(:delivery, error.message) }
+        end
+      end
     end
   end
 end
 
 Spree::Address.prepend Spree::EasyPost::AddressDecorator
 Spree::StockLocation.prepend Spree::EasyPost::AddressDecorator
+
