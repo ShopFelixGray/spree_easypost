e10b0f6ca81c8e73d8cb6ec1941683b1f4de1293
Jared Murphy <jaredcmurphy@gmail.com>
2018-03-29 12:47:43 -0400

    adds easypost_validate_address to app config decorator

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,4 +1,5 @@
 Spree::AppConfiguration.class_eval do
     preference :buy_postage, :boolean, default: false
     preference :auto_capture_on_postage_buy, :boolean, default: false # Captures payment for each shipment in Shipment#buy_easypost_rate callback, and buys rate when payment is authorized!
+    preference :easypost_validate_address, :boolean, default: true
 end
