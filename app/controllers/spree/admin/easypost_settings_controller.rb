d6f9b3eccd50412f618ca6e4c626db19d86305bd
hf2186 <hf2186@users.noreply.github.com>
2018-04-03 14:25:05 -0400

    Adding use only on backend optimization

--- app/controllers/spree/admin/easypost_settings_controller.rb
+++ app/controllers/spree/admin/easypost_settings_controller.rb
@@ -7,14 +7,14 @@ module Spree
       def update
         Spree::Config.buy_postage_when_shipped = easypost_settings_params[:buy_postage_when_shipped]
         Spree::Config.validate_address_with_easypost = easypost_settings_params[:validate_address_with_easypost]
-
+        Spree::Config.use_easypost_on_frontend = easypost_settings_params[:use_easypost_on_frontend]
         redirect_to :back
       end
 
       private
 
       def easypost_settings_params
-        params.require(:settings).permit(:buy_postage_when_shipped, :validate_address_with_easypost)
+        params.require(:settings).permit(:buy_postage_when_shipped, :validate_address_with_easypost, :use_easypost_on_frontend)
       end
     end
   end
