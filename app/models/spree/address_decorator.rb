3bdcfc75882090e47eb1030a017a7ae7108755ff
jaredmurphy <jaredcmurphy@gmail.com>
2018-08-20 14:01:53 -0400

    skips state and zipcode validations if using easypost validations

---
+++ app/models/spree/address_decorator.rb
@@ -0,0 +1,10 @@
+Spree::Address.class_eval do
+  validate :state_validate, :postal_code_validate, if: :use_spree_validations?
+
+  private
+
+  def use_spree_validations?
+    # if Spree::Config.validate_address_with_easypost is set to true then we do not want to run Spree's built-in validations
+    !Spree::Config.validate_address_with_easypost
+    end
+end
